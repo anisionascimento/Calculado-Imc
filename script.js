bc6d987e1012da5c54c6b07d9948d8a9c4c4ce2f
Anísio Gomes do Nascimento <ANISIOGNASCIMENTO@GMAIL.COM>
2023-01-18 23:03:49 -0300

    escript
    add button

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById ('button');
+const nome = document.getElementById ('nome');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+const resultado = document.getElementById('resultado');
+
+const GeTxt = (imc) =>{
+    if (imc > 40) return 'Obesidade grau III';
+    if (imc > 35) return 'Obesidade grau II';
+    if (imc > 30) return 'Obesidade grau I';
+    if (imc > 25) return 'Levemente acima do peso';
+    if (imc > 18) return 'Peso ideal';
+ 
+}
+
+
+const imCalc = () => {
+    const valorimc = (+peso.value / (+altura.value *+altura.value)).toFixed(1);
+    resultado.textContent = Getxt(valorimc);
+
+}
+button.addEventListener('click',imCalc);    
